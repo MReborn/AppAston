a6a410ca751926245ea2b14d804fa9d832c063b8
Mks <survival199910@yandex.ru>
2024-01-20 02:52:11 +0300

    Переименовал на более подходящее имя. Убрал сеттеры, которые не нужны

---
+++ src/main/java/com/maximbuza/appaston/dto/LoginAndRegistrationUserRequestDTO.java
@@ -0,0 +1,16 @@
+package com.maximbuza.appaston.dto;
+
+
+public class LoginAndRegistrationUserRequestDTO {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+    public String getPassword() {
+        return password;
+    }
+
+
+}
