11549bdba118bbedcb284efbcb1ce497a3251d38
Mks <survival199910@yandex.ru>
2024-01-19 14:45:16 +0300

    add sing up, sing in, getter users, changer password methods

--- src/main/java/com/maximbuza/appaston/controller/MyController.java
+++ src/main/java/com/maximbuza/appaston/controller/MyController.java
@@ -1,6 +1,32 @@
 package com.maximbuza.appaston.controller;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class MyController {
+
+    @RequestMapping("/showAllUsers")
+    public String showAllUsers(Model model) {
+        List<User> allUsers = userService.getAllEmployees();
+        model.addAttribute("allUsers", allUsers);
+        return "all-users";
+    }
+    @RequestMapping("/singUp")
+    public String singUpUser(Model model) {
+
+    }
+    @RequestMapping("/")
+    public String singInUser(Model model) {
+
+    }
+    @RequestMapping("/changePassword")
+    public String changePassword(Model model) {
+
+    }
+
+
 }
