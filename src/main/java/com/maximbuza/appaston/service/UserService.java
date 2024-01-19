976d56b50b5e719ab03e240b7e51cef4583c22b4
Mks <survival199910@yandex.ru>
2024-01-20 01:42:29 +0300

    добавил метод обращения сервиса к хранилищу насчет регистрации пользователя

--- src/main/java/com/maximbuza/appaston/service/UserService.java
+++ src/main/java/com/maximbuza/appaston/service/UserService.java
@@ -1,11 +1,13 @@
 package com.maximbuza.appaston.service;
 
+import com.maximbuza.appaston.dto.UserRegistrationRequestDTO;
+
 import java.util.Map;
 import java.util.Set;
 
 public interface UserService {
     public Set<Map.Entry<String,String>> getAllUsers();
-    public void singUpUser();
+    public String singUpUser(UserRegistrationRequestDTO userRegistrationRequestDTO);
 
     public void singInUser();
 
