ce5bb28f1a227521c8427d6787804f334ada0773
Mks <survival199910@yandex.ru>
2024-01-20 01:41:35 +0300

    доабвлен метод регистрации с проверкой существует ли пользователь если да, то добавить в хранилище и ответить юзеру что добавлен если нет, то сказать об этом и объяснить почему

--- src/main/java/com/maximbuza/appaston/storage/UserPasswordStorage.java
+++ src/main/java/com/maximbuza/appaston/storage/UserPasswordStorage.java
@@ -1,24 +1,46 @@
 package com.maximbuza.appaston.storage;
 
+import com.maximbuza.appaston.dto.UserRegistrationRequestDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.Set;
+
 @Component
 public class UserPasswordStorage {
-    HashMap<String, String> userAccount = new HashMap<>(){{put("Max","12345");put("Boot","11111");put("Var","54321");}};
+
+    HashMap<String, String> userAccounts = new HashMap<>() {{
+        put("Max", "12345");
+        put("Boot", "11111");
+        put("Var", "54321");
+    }};
+
 
     public boolean isUserExist(String username) {
-        return userAccount.containsKey(username);
+        return userAccounts.containsKey(username);
     }
 
     public boolean isPasswordCorrect(String username, String password) {
-        return userAccount.get(username).equals(password);
+        return userAccounts.get(username).equals(password);
     }
 
-    public Set<Map.Entry<String,String>> giveAllUser(){
-        return userAccount.entrySet();
+    public Set<Map.Entry<String, String>> giveAllUser() {
+        return userAccounts.entrySet();
+    }
+
+
+    public String signUpUser(UserRegistrationRequestDTO userRegistrationRequestDTO) {
+        String userPossible = userRegistrationRequestDTO.getUsername();
+        String passwordPossible = userRegistrationRequestDTO.getPassword();
+
+        if (!this.isUserExist(userPossible)) {
+            userAccounts.put(userPossible, passwordPossible);
+            return "User has been added:\n login: "+userPossible+"\npassword: "+passwordPossible;
+        } else {
+            return "Oh no! The user has already been added once";
+        }
+
     }
 }
