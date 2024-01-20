e3d03f19afc9ae4bf82a80a7c6bdad7f3e46e094
Mks <survival199910@yandex.ru>
2024-01-20 14:20:22 +0300

    Добавил метод обращающийся к хранилищу для смены пароля

--- src/main/java/com/maximbuza/appaston/service/UserService.java
+++ src/main/java/com/maximbuza/appaston/service/UserService.java
@@ -1,15 +1,17 @@
 package com.maximbuza.appaston.service;
 
 import com.maximbuza.appaston.dto.LoginAndRegistrationUserRequestDTO;
+import com.maximbuza.appaston.dto.ChangerPasswordRequestDTO;
 
 import java.util.Map;
 import java.util.Set;
 
 public interface UserService {
-    public Set<Map.Entry<String,String>> getAllUsers();
+    public String getAllUsers();
+
     public String singUpUser(LoginAndRegistrationUserRequestDTO loginAndRegistrationUserRequestDTO);
 
     public String singInUser(LoginAndRegistrationUserRequestDTO loginAndRegistrationUserRequestDTO);
 
-//    public User changePassword();
+    public String changePassword(ChangerPasswordRequestDTO changerPasswordRequestDTO);
 }
