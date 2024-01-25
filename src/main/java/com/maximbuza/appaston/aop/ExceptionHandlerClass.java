73b1375232d3d3aeec20a7951459f32b26c3516c
Mks <survival199910@yandex.ru>
2024-01-25 21:10:38 +0300

    Все exception хендлеры в один класс controllerAdvice

---
+++ src/main/java/com/maximbuza/appaston/aop/ExceptionHandlerClass.java
@@ -0,0 +1,34 @@
+package com.maximbuza.appaston.aop;
+
+import com.maximbuza.appaston.exception.BadDataException;
+import com.maximbuza.appaston.exception.ConflictException;
+import com.maximbuza.appaston.exception.NotFoundException;
+import com.maximbuza.appaston.exception.UnauthorizedException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class ExceptionHandlerClass {
+    @ExceptionHandler(BadDataException.class)
+    public ResponseEntity<String> handlerBadData(BadDataException badDataException) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(badDataException.getMessage());
+    }
+
+    @ExceptionHandler(ConflictException.class)
+    public ResponseEntity<String> conflictHandler(ConflictException conflictException) {
+        return ResponseEntity.status(409).body(conflictException.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> notFoundHandler(NotFoundException notFoundException) {
+        return ResponseEntity.status(404).body(notFoundException.getMessage());
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<String> unauthorizedHandler(UnauthorizedException unauthorizedException) {
+        return ResponseEntity.status(401).body(unauthorizedException.getMessage());
+    }
+
+}
