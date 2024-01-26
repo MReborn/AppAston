27ae63bb8eba7af5745cf896bf9302babdee4d5a
Mks <survival199910@yandex.ru>
2024-01-26 17:52:52 +0300

    Сменил имя

--- src/main/java/com/maximbuza/appaston/configuration/MyConfiguration.java
+++ src/main/java/com/maximbuza/appaston/configuration/MyConfiguration.java
@@ -1,10 +1,12 @@
 package com.maximbuza.appaston.configuration;
 
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-@org.springframework.context.annotation.Configuration // обозначаем как конфигурацию
+@Configuration // обозначаем как конфигурацию
 @ComponentScan(basePackages = "com.maximbuza.appaston") // где нужно искать компоненты
 @EnableWebMvc // включаем MVC для обработки запросов HTTP
-public class Configuration {
+public class MyConfiguration {
+
 }
