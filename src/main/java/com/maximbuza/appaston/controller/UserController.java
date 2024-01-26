c784a52f147bc3ec3bf16625f64fd4ac6add0c97
Mks <survival199910@yandex.ru>
2024-01-26 17:54:10 +0300

    Расширяю возможности контроллер, подключая тестовый сервис который работает с данными из бд

--- src/main/java/com/maximbuza/appaston/controller/UserController.java
+++ src/main/java/com/maximbuza/appaston/controller/UserController.java
@@ -2,20 +2,26 @@ package com.maximbuza.appaston.controller;
 
 
 import com.maximbuza.appaston.dto.User;
+import com.maximbuza.appaston.entity.UserEntity;
+import com.maximbuza.appaston.service.ServiceBD;
 import com.maximbuza.appaston.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/api") // к этому контроллеру обращаться через /api
 public class UserController {
-    public UserController(@Autowired UserService userService) {
+    public UserController(@Autowired UserService userService, @Autowired ServiceBD serviceBD) {
         this.userService = userService;
+        this.serviceBD = serviceBD;
     } // прикрепили сервис к контроллеру
 
     private final UserService userService;
+    private final ServiceBD serviceBD;
 
 
     @GetMapping("/getAllUsers") // по этому адресу можно вызвать get запрос и получить список юзеров
@@ -37,6 +43,11 @@ public class UserController {
     public ResponseEntity<String> changePassword(@RequestBody User user) { // образует контейнер данных пользователя и передает сервису
         return ResponseEntity.ok(userService.changePassword(user));
     }
+
+    @GetMapping("/getAllUsersFromBD") // по этому адресу можно вызвать get запрос и получить список юзеров
+    public ResponseEntity<String> getAllUsersFromBd() {
+        return ResponseEntity.ok(serviceBD.getAllUsersFromBd());
+    } // делегирует запрос сервису
 }
 
 
