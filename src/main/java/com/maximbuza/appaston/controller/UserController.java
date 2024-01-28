3a64083121fe330ac1225064013265dd22b59e94
Mks <survival199910@yandex.ru>
2024-01-28 15:47:36 +0300

    Приложение больше не работает с локальным хранилищем

--- src/main/java/com/maximbuza/appaston/controller/UserController.java
+++ src/main/java/com/maximbuza/appaston/controller/UserController.java
@@ -4,7 +4,6 @@ package com.maximbuza.appaston.controller;
 import com.maximbuza.appaston.dto.ChangePasswordDTO;
 import com.maximbuza.appaston.dto.UserDTO;
 import com.maximbuza.appaston.service.ServiceBD;
-import com.maximbuza.appaston.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -13,34 +12,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api") // к этому контроллеру обращаться через /api
 public class UserController {
-    public UserController(@Autowired UserService userService, @Autowired ServiceBD serviceBD) {
-        this.userService = userService;
+    public UserController(@Autowired ServiceBD serviceBD) {
         this.serviceBD = serviceBD;
     } // прикрепили сервис к контроллеру
 
-    private final UserService userService;
     private final ServiceBD serviceBD;
-
-
-    @GetMapping("/getAllUsers") // по этому адресу можно вызвать get запрос и получить список юзеров
-    public ResponseEntity<String> getAllUsers() {
-        return ResponseEntity.ok(userService.getAllUsers());
-    } // делегирует запрос сервису
-
-    @PostMapping("/signUpUser") //регистрация юзера
-    public ResponseEntity<String> signUpUser(@RequestBody UserDTO user) { // образует контейнер данных пользователя и передает сервису
-        return ResponseEntity.status(201).body(userService.signUpUser(user));
-    }
-
-    @PostMapping("/signInUser") //вход юзера
-    public ResponseEntity<String> signInUser(@RequestBody UserDTO user) { // образует контейнер данных пользователя и передает сервису
-        return ResponseEntity.ok(userService.signInUser(user));
-    }
-
-    @PostMapping("/changePassword") //смена пароля
-    public ResponseEntity<String> changePassword(@RequestBody ChangePasswordDTO changePasswordDTO) { // образует контейнер данных пользователя и передает сервису
-        return ResponseEntity.ok(userService.changePassword(changePasswordDTO));
-    }
 
     @GetMapping("/getAllUsersFromBD") // по этому адресу можно вызвать get запрос и получить список юзеров
     public ResponseEntity<String> getAllUsersFromBd() {
--- src/main/java/com/maximbuza/appaston/storage/Storage.java
+++
@@ -1,9 +0,0 @@
-package com.maximbuza.appaston.storage;
-
-import org.springframework.stereotype.Repository;
-
-import java.util.HashMap;
-@Repository
-public class Storage {
-    public static HashMap<String, String> userAccounts = new HashMap<>(); // здесь хранятся все данные о пользователях
-}
