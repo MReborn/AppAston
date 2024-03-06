57ea992e935899967eb828aeefed42887e52cdac
Mikhail Khlebnikov <prodbyrotiez@gmail.com>
2024-03-07 00:29:36 +0300

    добавлен lombok

--- src/main/java/com/maximbuza/appaston/dto/ChangePasswordDTO.java
+++ src/main/java/com/maximbuza/appaston/dto/ChangePasswordDTO.java
@@ -1,30 +1,12 @@
 package com.maximbuza.appaston.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ChangePasswordDTO { // нужен для смены пароля пользователя
     private String username;
     private String oldPassword;
     private String newPassword;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getOldPassword() {
-        return oldPassword;
-    }
-
-    public void setOldPassword(String oldPassword) {
-        this.oldPassword = oldPassword;
-    }
-
-    public String getNewPassword() {
-        return newPassword;
-    }
-
-    public void setNewPassword(String newPassword) {
-        this.newPassword = newPassword;
-    }
 }
--- src/main/java/com/maximbuza/appaston/dto/UserDTO.java
+++ src/main/java/com/maximbuza/appaston/dto/UserDTO.java
@@ -1,24 +1,13 @@
 package com.maximbuza.appaston.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class UserDTO {
     private String username;
     private String password;
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 
     @Override
     public String toString() {
